ec7f28cd7dceeedaf1577e1c2aa7793e97f37fe7
dimassumantri <82698723+dimassumantri@users.noreply.github.com>
2021-04-23 09:38:28 +0700

    init_func update

--- app.py
+++ app.py
@@ -85,10 +85,13 @@ async def messages(req: Request) -> Response:
     return Response(status=HTTPStatus.OK)
 
 
-APP = web.Application(middlewares=[aiohttp_error_middleware])
-APP.router.add_post("/api/messages", messages)
+def init_func(argv):
+    APP = web.Application(middlewares=[aiohttp_error_middleware])
+    APP.router.add_post("/api/messages", messages)
+    return APP
 
 if __name__ == "__main__":
+    APP = init_func(None)
     try:
         web.run_app(APP, host="localhost", port=CONFIG.PORT)
     except Exception as error:
